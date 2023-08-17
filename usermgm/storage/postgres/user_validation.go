bd1a3f589ce8dfeef5bd89554c360cf9d911b804
iamsabbiralam <sabbiriph4@gmail.com>
2023-08-18 01:33:35 +0600

    user profile update server side
    1. pass create user information test case
    2. add user validation file
    3. change user information migration file
    4. add log in storage

---
+++ usermgm/storage/postgres/user_validation.go
@@ -0,0 +1,32 @@
+package postgres
+
+import (
+	"context"
+
+	validation "github.com/go-ozzo/ozzo-validation/v4"
+	"github.com/pkg/errors"
+
+	"github.com/iamsabbiralam/restora/usermgm/storage"
+)
+
+func (s *Storage) CreateUserInformationValidation(ctx context.Context, req *storage.UserInformation) error {
+	if req == nil {
+		return errors.New("invalid request")
+	}
+
+	required := validation.Required
+	if err := validation.ValidateStruct(req,
+		validation.Field(&req.FirstName, required),
+		validation.Field(&req.LastName, required),
+		validation.Field(&req.Image, required),
+		validation.Field(&req.Mobile, required),
+		validation.Field(&req.Gender, required),
+		validation.Field(&req.Address, required),
+		validation.Field(&req.City, required),
+		validation.Field(&req.Country, required),
+	); err != nil {
+		return err
+	}
+
+	return nil
+}
